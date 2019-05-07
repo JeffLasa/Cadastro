ea00f3f31d104149a16394ac592a34a31a34d7a9
JeffLasa <jeff.lasa@hotmail.com>
2019-05-06 22:50:59 -0300

    correção do TestaCadastro

--- src/CadastraPessoas.java
+++ src/CadastraPessoas.java
@@ -5,8 +5,26 @@ import java.util.List;
 public class CadastraPessoas {
 
     private int quantidadeDePessoas;
-    private List<Pessoa> ListaDePessoas = new ArrayList();
+    private List<Pessoa> listaDePessoas = new ArrayList();
 
+    public void cadastrarPessoa(Pessoa pessoa){
+        listaDePessoas.add(pessoa);
+        quantidadeDePessoas = quantidadeDePessoas + 1;
+    }
+
+    public void imprimeCadastro(){
+        for(Pessoa pessoa : listaDePessoas){
+            pessoa.imprimeDados();
+        }
+    }
+
+    public int getQuantidadeDePessoas() {
+        return quantidadeDePessoas;
+    }
+
+    public void setQuantidadeDePessoas(int quantidadeDePessoas) {
+        this.quantidadeDePessoas = quantidadeDePessoas;
+    }
 
 
 
--- src/TestaCadastro.java
+++ src/TestaCadastro.java
@@ -7,17 +7,31 @@ public class TestaCadastro {
         cliente.setCodigo(0001);
         Data dataCliente = new Data(03,02,1988);
         cliente.setDataNascimento(dataCliente);
-        cliente.imprimeDados();
 
         Funcionario funcionario1 = new Funcionario();
         funcionario1.setNome("ALfredo das Neves");
         funcionario1.setSalario(10000F);
         Data datafuncionario = new Data( 25,03,1970);
         funcionario1.setDataNascimento(datafuncionario);
-        funcionario1.imprimeDados();
+
 
         Gerente gerente1 = new Gerente();
-        gerente1
+        gerente1.setNome("Pablo Neruda");
+        gerente1.setSalario(15000F);
+        gerente1.setArea("Vendas");
+        Data dataGerente = new Data(12,02,1975);
+        gerente1.setDataNascimento(dataGerente);
+
+
+        // Criar Cadastro
+        CadastraPessoas cadastro = new CadastraPessoas();
+        cadastro.cadastrarPessoa(cliente);
+        cadastro.cadastrarPessoa(funcionario1);
+        cadastro.cadastrarPessoa(gerente1);
+
+        System.out.println("Quantidade de pessoas cadastradas: "+cadastro.getQuantidadeDePessoas());
+
+        cadastro.imprimeCadastro();
 
 
 
